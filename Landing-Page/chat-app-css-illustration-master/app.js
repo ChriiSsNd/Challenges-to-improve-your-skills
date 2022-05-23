4529627a20ac38b00ac4437fbdde503fe6630332
chris ndouassi <chrisndouassi@gmail.com>
2022-05-23 22:21:57 +0200

    Challenge 2 finished

---
+++ Landing-Page/chat-app-css-illustration-master/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $(".poster").addClass("animate__animated animate__zoomIn");
+    $("h1").addClass("animate__animated animate__lightSpeedInRight");
+    $(".text").addClass("animate__animated animate__bounceInUp");
+});
