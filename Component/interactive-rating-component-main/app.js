0b1e6fb2411ddc3548187c1ecf0d70705352f9ff
chris ndouassi <chrisndouassi@gmail.com>
2022-05-28 14:15:12 +0200

    challenge 3 finished

---
+++ Component/interactive-rating-component-main/app.js
@@ -0,0 +1,24 @@
+const rates = document.querySelectorAll('.rate');
+const rating = document.querySelector('.btn');
+const rate = document.querySelector('.container__rate')
+const success = document.querySelector('.container__success')
+const successText = document.querySelector('.successText');
+
+chooseRate(rates);
+
+rating.addEventListener('click', function(){
+    rate.style.display = 'none';
+    success.setAttribute("class","container__success animate__animated animate__zoomIn");
+    success.style.display = 'block';
+})
+
+function chooseRate(rate) {
+    rate.forEach((element, index) => {
+        element.addEventListener('click', function() {
+            element.style.background = 'hsl(217, 12%, 63%)';
+            element.style.color = 'hsl(0, 0%, 100%)';
+            successText.innerHTML = `You selected ${index + 1} out of 5`
+        });
+        // element.removeEventListener();
+    });
+};
