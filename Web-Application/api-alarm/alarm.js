52c168062dd02d8d16a61c8f637e4d3991742ae0
chris ndouassi <chrisndouassi@gmail.com>
2022-06-21 20:49:13 +0200

    Project Api Alarm

---
+++ Web-Application/api-alarm/alarm.js
@@ -0,0 +1,22 @@
+const namePerson = document.querySelector('#name');
+const delay = document.querySelector('#delay');
+const button = document.querySelector('#set-alarm');
+const output = document.querySelector('.alarm__message');
+
+button.addEventListener('click', () => {
+    alarm(namePerson.value, delay.value)
+        .then(message => output.textContent = message)
+        .catch(error => output.textContent = `Couldn't set alarm: ${error}`);
+});
+
+function alarm(person, delay) {
+    return new Promise((resolve, reject) => {
+        if (delay < 0) {
+            throw new Error('Alarm delay must not be negative');
+        };
+
+        window.setTimeout(() => {
+            resolve(`Wake up ${person}!`);
+        }, delay);
+    });
+}
